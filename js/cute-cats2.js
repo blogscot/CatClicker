505ffdf9dd83091782b04741cdfad52ba3c6e5e9
blogscot <iain.diamond@gmail.com>
2015-05-06 12:18:03 +0100

    added admin form and data binding

--- js/cute-cats2.js
+++ js/cute-cats2.js
@@ -2,6 +2,7 @@
 
   var model = {
     currentCat: 0,
+    adminVisible: false,
     cats: [
   {
     url: 'images/kitten.jpg',
@@ -35,6 +36,7 @@
     init: function() {
       catListView.init();
       catView.init();
+      adminView.init();
     },
     getCats: function() {
       return model.cats;
@@ -48,27 +50,43 @@
     getCatName: function(currentCat) {
       return model.cats[currentCat].name;
     },
+    setCatName: function(name) {
+      model.cats[this.getCurrentCat()].name = name;
+    },
     getCatUrl: function(currentCat) {
       return model.cats[currentCat].url;
+    },
+    setCatUrl: function(url) {
+      model.cats[this.getCurrentCat()].url = url;
     },
     getCatCount: function(currentCat) {
       return model.cats[currentCat].count;
     },
+    setCatCount: function(count) {
+      model.cats[this.getCurrentCat()].count = count;
+    },
     incrementCount: function(currentCat) {
       model.cats[currentCat].count++;
+    },
+    renderCatView: function() {
+      catView.render(this.getCurrentCat());
+    },
+    renderAdminView: function() {
+      adminView.render();
     }
   };
 
   var catView = {
     init: function() {
       var currentCat = octopus.getCurrentCat();
-      this.display(currentCat);
+      this.render(currentCat);
       var that = this;   // this (i.e. catView) gets rebound inside the click handler
 
       // Display the selected cat
       $('li').click(function() {
         currentCat = $(this).attr('data-item');
-        that.display(currentCat);
+        that.render(currentCat);
+        octopus.renderAdminView();
       });
 
       // Update the click count
@@ -76,10 +94,11 @@
         var currentCat = octopus.getCurrentCat();
 
         octopus.incrementCount(currentCat);
+        octopus.renderAdminView();
         $('#click-count').text(octopus.getCatCount(currentCat));
       });
     },
-    display: function(currentCat) {
+    render: function(currentCat) {
       var catName = octopus.getCatName(currentCat);
       var catUrl = octopus.getCatUrl(currentCat);
       var catCount = octopus.getCatCount(currentCat);
@@ -110,6 +129,48 @@
         catName = octopus.getCatName(i);
         this.catListElem.append("<li data-item='"+i+"''>"+catName+"</li>");
       }
+    }
+  };
+
+  var adminView = {
+    init: function() {
+      that = this;
+      $('form').hide();
+
+      $('#admin-btn').click(function() {
+        that.render();
+
+        $('form').toggle();
+      });
+
+      $('#catForm').submit(function(e) {
+        e.preventDefault();
+        var catName = $('input#catName').val();
+        var catUrl = $('input#catUrl').val();
+        var catClicks = $('input#catClicks').val();
+
+        // Save the current form values
+        octopus.setCatName(catName);
+        octopus.setCatUrl(catUrl);
+        octopus.setCatCount(catClicks);
+        octopus.renderCatView();
+
+        $('form').toggle();
+      });
+      // The cancel button is pressed
+      $('button[type="button"]').click(function() {
+        $('form').hide();
+      });
+    },
+    render: function() {
+      var currentCat = octopus.getCurrentCat();
+      var catName = octopus.getCatName(currentCat);
+      var catUrl = octopus.getCatUrl(currentCat);
+      var catClicks = octopus.getCatCount(currentCat);
+
+      $('input#catName').val(catName);
+      $('input#catUrl').val(catUrl);
+      $('input#catClicks').val(catClicks);
     }
   };
 
